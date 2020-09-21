484cdf048dd7c7a2446408c719a9f24850266ddc
Camron Bartlow <csbartlow99@gmail.com>
2020-09-20 23:49:41 -0500

    Initial commit

---
+++ R/quadPlot.R
@@ -0,0 +1,12 @@
+#' @title A function for plotting a quadratic
+#'
+#' @param x The diameter
+#'
+#' @return Response i.e. estimated Height
+#' @export
+#'
+#' @examples
+#' \dontrun{quad.lm<-lm(y~x,data=df); myplot(x=15, qlm=quad.lm)}
+quadPlot = function(x, qlm) {
+  qlm$coef[1] + qlm$coef[2] * x  + qlm$coef[3] * x^2
+}
