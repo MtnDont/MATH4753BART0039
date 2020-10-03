2b50d3c0115fc4ede8279b426920f838f5ff7874
Camron Bartlow <csbartlow99@gmail.com>
2020-10-02 21:28:34 -0500

    Added myncurve method that prints probability curve

---
+++ R/myncurve.R
@@ -0,0 +1,21 @@
+#' @title Function for printing probability on curves
+#'
+#' @param mu Mean
+#' @param sigma Standard Deviation
+#' @param a
+#'
+#' @return Displays a curve with a shaded area between the curve and the x axis
+#' from -inf to x = a, and prints the probability P(X<=a)
+#' @export
+#'
+#' @examples
+myncurve = function(mu, sigma, a) {
+  curve(dnorm(x, mean=mu, sd=sigma),
+        xlim = c(mu - 3 * sigma, mu + 3 * sigma))
+  xcurve = seq(mu - 3 * sigma,a,length=1000)
+  ycurve = dnorm(xcurve,mu,sigma)
+  polygon(c(mu - 3 * sigma,xcurve,a), c(0,ycurve,0), col = "Red")
+  prob = pnorm(a, mu, sigma) - pnorm(mu - 3 * sigma, mu, sigma)
+  prob = round(prob,4)
+  prob
+}
